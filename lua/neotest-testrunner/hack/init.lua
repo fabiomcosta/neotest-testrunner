0fa3db3ddf22c47628c7500d1e7b892e8cf9cedd
fabs <fabs@fb.com>
2025-09-29 21:16:07 -0400

    ....

--- lua/neotest-testrunner/hack/init.lua
+++ lua/neotest-testrunner/hack/init.lua
@@ -1,6 +1,7 @@
 local lib = require("neotest.lib")
 local logger = require("neotest.logging")
 local utils = require("neotest-testrunner.utils")
+local nio = require('nio')
 
 local config = {
   cmd = 't',
@@ -26,6 +27,7 @@ function NeotestAdapter.root(dir)
 end
 
 ---Filter directories when searching for test files
+---We don't use discovery, so simply returning false.
 ---@async
 ---@param name string Name of directory
 ---@return boolean
@@ -72,13 +74,15 @@ end
 ---@param args neotest.RunArgs
 ---@return neotest.RunSpec | nil
 function NeotestAdapter.build_spec(args)
+  logger.info("BUILD_SPEC ARGS", args.tree:children())
   local position = args.tree:data()
 
   if position.type == 'directory' or position.type == 'namespace' then
     return error('Position type: ' .. position.type .. ' not supported')
   end
 
-  local command = { program = config.cmd, position.path }
+  local log_path = nio.fn.tempname()
+  local command = { config.cmd, position.path, '--event-log-file', log_path }
 
   if position.type == "test" then
     local filter_args = {
@@ -94,10 +98,57 @@ function NeotestAdapter.build_spec(args)
   ---@type neotest.RunSpec
   return {
     command = command,
-    context = { position = { id = position.id, type = position.type } },
+    context = {
+      position = { id = position.id, type = position.type },
+      log = { path = log_path }
+    },
     env = args.env,
   }
 end
+
+
+    ---@param args neotest.RunArgs
+    ---@return neotest.RunSpec
+    -- build_spec = function(args)
+    --   local stream_path = nio.fn.tempname()
+    --   lib.files.write(stream_path, "")
+    --   local stream_data, stop_stream = lib.files.stream_lines(stream_path)
+    --
+    --   ---@type neotest.RunSpec
+    --   return {
+    --     context = {
+    --       results_path = results_path,
+    --       stop_stream = stop_stream,
+    --     },
+    --     stream = function()
+    --       return function()
+    --         local lines = stream_data()
+    --         local results = {}
+    --         for _, line in ipairs(lines) do
+    --           local result = vim.json.decode(line, { luanil = { object = true } })
+    --           results[result.name] = result.result
+    --         end
+    --         return results
+    --       end
+    --     end,
+    --   }
+    -- end,
+    -- ---@param spec neotest.RunSpec
+    -- ---@param result neotest.StrategyResult
+    -- ---@return neotest.Result[]
+    -- results = function(spec, result)
+    --   spec.context.stop_stream()
+    --   local success, data = pcall(lib.files.read, spec.context.results_path)
+    --   if not success then
+    --     data = "{}"
+    --   end
+    --   local results = vim.json.decode(data, { luanil = { object = true } })
+    --   for _, pos_result in pairs(results) do
+    --     result.output_path = pos_result.output_path
+    --   end
+    --   return results
+    -- end,
+
 
 ---@async
 ---@param spec neotest.RunSpec
@@ -105,8 +156,10 @@ end
 ---@param tree neotest.Tree
 ---@return neotest.Result[]
 function NeotestAdapter.results(spec, result, tree)
-  local position = spec.context.position
+  logger.info("RESULTS SPEC", spec)
+  logger.info("RESULTS RESULT", result)
 
+  local position = spec.context.position
 
   if result.code == 0 then
     return {
@@ -115,6 +168,21 @@ function NeotestAdapter.results(spec, result, tree)
       }
     }
   end
+
+  -- log file structure
+  --
+  -- first line:
+  -- {"type": "run_id", "id":2251800129442060}
+  -- Then one line for each test run:
+  -- {
+  --   "test_name":"ClassName::testMethodName",
+  --   "config":{},
+  --   "status":3, <- 1:PASS, 2:FAIL, 3:SKIP
+  --   "status_detailed":401, <- not sure what this is...
+  --   "details":null,
+  --   "result_id":"2251800129442060.844425137975780.1759189991",
+  --   "duration_secs":22.877155464
+  -- }
 
   if position.type == "file" then
     return {
@@ -129,10 +197,10 @@ function NeotestAdapter.results(spec, result, tree)
   -- When the type is "test", the "t" command has what seems to be a bug
   -- where it returns code=1 even when all filtered tests passed.
   if position.type == "test" then
-    local output_file = result.output
-    local ok, output_content = pcall(lib.files.read, output_file)
+    local output_path = result.output
+    local ok, output_content = pcall(lib.files.read, output_path)
     if not ok then
-      logger.error("Could not get test results", output_file)
+      logger.error("Could not get test results", output_path)
       return {}
     end
     local any_test_failed = string.find(output_content, '[(]FAILED [(]%d+[)][)]') ~= nil
